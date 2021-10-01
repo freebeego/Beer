8ed207e7d8ed795acf18a587c3462792ece6e826
freebeego <freebeego@yandex.ru>
2021-10-01 11:23:15 +0300

    feat: implement routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { FC } from 'react'
+
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import { routes } from '@constants/routes'
+
+import Catalog from '@components/Catalog'
+import Details from '@components/Details'
 
 import s from './styles.module.styl'
 
-function App() {
-  return (
-    <div className={s.test}>
-      <div className={s.testin}></div>
-    </div>
-  )
-}
+const App: FC = () => (
+  <div className={s.container}>
+    <Switch>
+      <Route exact path={routes.root}>
+        <Catalog />
+      </Route>
+
+      <Route exact path={routes.details}>
+        <Details />
+      </Route>
+
+      <Route path={routes.root}>
+        <Redirect to={routes.root} />
+      </Route>
+    </Switch>
+  </div>
+)
 
 export default App
